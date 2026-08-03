edda78876e8b3bdd769ba88c63384d736732b916
AdeelFateh123 <hafiz@avantisprivacy.com>
2026-08-03 15:01:23 +0500

    Update api.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "http://osint-platform-alb-1887306171.us-east-1.elb.amazonaws.com";
 export const SCRAPER_URL = "http://localhost:8001";
